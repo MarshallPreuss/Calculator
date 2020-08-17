1da08d55399a4fb5c74396e8ca3fd5c996bf0bcd
MarshallPreuss <marshallpreuss@gmail.com>
2020-08-17 16:43:44 -0400

    more

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Calc from './Components/Calc';
+import Pad from './Components/Pad';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+
+    if(button === "="){
+      this.calculate()
+    }
+    else if(button === "C"){
+      this.reset()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  calculate = () => {
+    let checkResult = ''
+    if(this.state.result.includes('--')){
+      checkResult = this.state.result.replace('--','+')
+    }
+    else {
+      checkResult = this.state.result
+    }
+
+    try{
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+    }
+  }
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  }
+
+  render() {
+    return(
+      <div className="body">
+        <Calc result={this.state.result}/>
+        <Pad onClick={this.onClick}/>
+      </div>
+    )
+  }
 }
 
 export default App;
